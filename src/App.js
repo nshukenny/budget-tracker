2c41c54eca7cc6a717a7d5eaa583a8317b7dad02
Nshuti Kenny Herve <nshukenny@gmail.com>
2023-09-24 05:17:20 -0700

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@mui/material";
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Report from './Report';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Feed} />
+          <Route path="report" element={<Report />} />
+        </Switch>
+      </Router>
+    </Box>
   );
 }
-
 export default App;
